0fccc18b09193a563f727004c14a0f9b9eb127b7
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2025-08-28 11:24:04 +0100

    Add personal history of cancer to PLCO calculator

--- lung_cancer_screening/calculators/plco.py
+++ lung_cancer_screening/calculators/plco.py
@@ -8,11 +8,13 @@ class Plco:
     BMI_COEFFICIENT = Decimal('-0.0274194')
     BMI_CENTRED_OR_REFERENT_REF_GROUP = Decimal('27')
     COPD_ENPHYSEMA_OR_CHRONIC_BRONCHITIS_COEFFICIENT = Decimal('0.3553063')
+    PERSONAL_HISTORY_OF_CANCER_COEFFICIENT = Decimal('0.4589971')
 
-    def __init__(self, age=None, bmi=None, copd_enphysema_or_chronic_bronchitis=None):
+    def __init__(self, age=None, bmi=None, copd_enphysema_or_chronic_bronchitis=None, personal_history_of_cancer=None):
         self.age = Decimal(str(age or 0))
         self.bmi = Decimal(str(bmi or 0))
         self.copd_enphysema_or_chronic_bronchitis = copd_enphysema_or_chronic_bronchitis
+        self.personal_history_of_cancer = personal_history_of_cancer
 
     def age_in_years_contribution_to_estimate(self):
         return (self.age - self.AGE_CENTRED_OR_REFERENT_REF_GROUP) * self.AGE_COEFFICIENT
@@ -26,6 +28,14 @@ class Plco:
                 "copd_enphysema_or_chronic_bronchitis must be true or false")
 
         return self.copd_enphysema_or_chronic_bronchitis * self.COPD_ENPHYSEMA_OR_CHRONIC_BRONCHITIS_COEFFICIENT
+
+
+    def personal_history_of_cancer_contribution_to_estimate(self):
+        if self.personal_history_of_cancer is None:
+            raise self.InvalidValueError(
+                "personal_history_of_cancer must be true or false")
+
+        return self.personal_history_of_cancer * self.PERSONAL_HISTORY_OF_CANCER_COEFFICIENT
 
     class InvalidValueError(Exception):
         pass
