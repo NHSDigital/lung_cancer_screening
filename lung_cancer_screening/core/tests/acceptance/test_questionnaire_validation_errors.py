e8b890af115b7fb9a8e46340cc5a2c6bc2dff247
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2025-10-02 16:33:42 +0100

    PPHA-259: Add custom error messages to dob form

--- lung_cancer_screening/core/tests/acceptance/test_questionnaire_validation_errors.py
+++ lung_cancer_screening/core/tests/acceptance/test_questionnaire_validation_errors.py
@@ -3,8 +3,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from playwright.sync_api import sync_playwright, expect
 
 from .helpers.user_interaction_helpers import (
-    fill_in_and_submit_participant_id,
-    fill_in_and_submit_smoking_elligibility
+    fill_in_and_submit_participant_id
 )
 
 class TestQuestionnaireValidationErrors(StaticLiveServerTestCase):
@@ -22,26 +21,19 @@ class TestQuestionnaireValidationErrors(StaticLiveServerTestCase):
         cls.browser.close()
         cls.playwright.stop()
 
-    def test_full_questionaire_user_journey_with_validation_errors(self):
+    def test_date_of_birth_validation_errors(self):
         participant_id = '123'
 
         page = self.browser.new_page()
         page.goto(f"{self.live_server_url}/start")
-
         fill_in_and_submit_participant_id(page, participant_id)
-        fill_in_and_submit_smoking_elligibility(page, 'Yes, I currently smoke')
+        page.goto(f"{self.live_server_url}/date-of-birth")
 
         expect(page.locator("legend")).to_have_text(
             "What is your date of birth?")
 
-        page.get_by_label("Day").fill("100")
-        page.get_by_label("Month").fill("100")
-        page.get_by_label("Year").fill("some string")
-
         page.click("text=Continue")
 
-        expect(page).to_have_url(f"{self.live_server_url}/date-of-birth")
-
         expect(page.locator(".nhsuk-error-message")).to_contain_text(
-            "Day should be between 1 and 31"
+            "Enter your date of birth."
         )
--- lung_cancer_screening/questions/tests/unit/forms/test_date_of_birth_form.py
+++ lung_cancer_screening/questions/tests/unit/forms/test_date_of_birth_form.py
@@ -8,7 +8,7 @@ class TestDateOfBirthForm(TestCase):
     def setUp(self):
         self.participant = Participant.objects.create(unique_id="1234567890")
 
-    def test_is_valid(self):
+    def test_is_valid_when_a_valid_date_is_provided(self):
         form = DateOfBirthForm(
             participant=self.participant,
             data={
@@ -19,17 +19,53 @@ class TestDateOfBirthForm(TestCase):
         )
         self.assertTrue(form.is_valid())
 
-    def test_is_invalid(self):
+    def test_is_invalid_when_no_date_is_provided(self):
         form = DateOfBirthForm(
             participant=self.participant,
             data={
-                "date_of_birth_0": 100,
-                "date_of_birth_1": 100,
-                "date_of_birth_2": 2025
+                "date_of_birth_0": "",
+                "date_of_birth_1": "",
+                "date_of_birth_2": ""
             }
         )
 
         self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["date_of_birth"],
+            ["Enter your date of birth."]
+        )
+
+    def test_is_invalid_when_a_partial_date_is_provided(self):
+        form = DateOfBirthForm(
+            participant=self.participant,
+            data={
+                "date_of_birth_0": "10",
+                "date_of_birth_1": "",
+                "date_of_birth_2": ""
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["date_of_birth"],
+            ["Enter your full date of birth."]
+        )
+
+    def test_is_invalid_when_an_invalid_date_is_provided(self):
+        form = DateOfBirthForm(
+            participant=self.participant,
+            data={
+                "date_of_birth_0": "31",
+                "date_of_birth_1": "02",
+                "date_of_birth_2": "2025"
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["date_of_birth"],
+            ["Date of birth must be a real date."]
+        )
 
     def test_returns_a_date_type(self):
         form = DateOfBirthForm(
