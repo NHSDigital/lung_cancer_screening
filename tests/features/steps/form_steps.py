af868cbd2306777e6133d9f9879367c622f81cd0
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2025-11-18 15:42:20 +0000

    Add feature spec for age range exit

--- tests/features/steps/form_steps.py
+++ tests/features/steps/form_steps.py
@@ -10,3 +10,11 @@ def when_I_submit_my_participant_id(context):
 def when_I_fill_in_and_submit_my_smoking_status(context, smoking_status):
     context.page.get_by_label(smoking_status).check()
     context.page.click("text=Continue")
+
+@when('I fill in and submit my date of birth with "{date_of_birth}"')
+def when_I_fill_in_and_submit_my_date_of_birth(context, date_of_birth):
+    day, month, year = date_of_birth.split('-')
+    context.page.get_by_label('Day').fill(day)
+    context.page.get_by_label('Month').fill(month)
+    context.page.get_by_label('Year').fill(year)
+    context.page.click("text=Continue")
