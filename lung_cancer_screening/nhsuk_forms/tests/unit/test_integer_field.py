84e89dd0590db6f9ee475e996fb8d5d6100e5f85
Jamie Falcus <50366804+jamiefalcus@users.noreply.github.com>
2025-11-25 12:05:13 +0000

    Add suffix to integer field

--- lung_cancer_screening/nhsuk_forms/tests/unit/test_integer_field.py
+++ lung_cancer_screening/nhsuk_forms/tests/unit/test_integer_field.py
@@ -3,11 +3,10 @@ from django.forms import Form
 from ...integer_field import IntegerField
 
 
-class TestForm(Form):
-    field = IntegerField(label="Abc", initial=1, max_value=10)
-
 class TestIntegerField(TestCase):
     def test_renders_nhs_input(self):
+        class TestForm(Form):
+            field = IntegerField(label="Abc", initial=1, max_value=10)
         self.assertHTMLEqual(
             TestForm()["field"].as_field_group(),
             """
@@ -19,3 +18,20 @@ class TestIntegerField(TestCase):
             """,
         )
 
+    def test_renders_nhs_input_with_suffix(self):
+        class TestForm(Form):
+            field = IntegerField(label="Abc", initial=1, max_value=10, suffix="suffix")
+        self.assertHTMLEqual(
+            TestForm()["field"].as_field_group(),
+            """
+            <div class="nhsuk-form-group">
+                <label class="nhsuk-label" for="id_field">
+                    Abc
+                </label>
+                <div class="nhsuk-input__wrapper">
+                    <input class="nhsuk-input" id="id_field" name="field" type="number" value="1">
+                    <div class="nhsuk-input__suffix" aria-hidden="true">suffix</div>
+                </div>
+            </div>
+            """,
+        )
