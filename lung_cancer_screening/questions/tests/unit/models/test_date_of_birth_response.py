e9ddc433d6a75a7cbc83bb287ed41e3ed834046f
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2025-12-22 09:40:55 +0000

    Move DateOfBirthResponse to its own model

---
+++ lung_cancer_screening/questions/tests/unit/models/test_date_of_birth_response.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from datetime import date
+
+from ...factories.response_set_factory import ResponseSetFactory
+from ....models.date_of_birth_response import DateOfBirthResponse
+
+
+class TestDateOfBirthResponse(TestCase):
+    def test_has_response_set_as_foreign_key(self):
+        response_set = ResponseSetFactory()
+        response = DateOfBirthResponse.objects.create(
+            response_set=response_set,
+            value=date(2000, 9, 8)
+        )
+
+        self.assertEqual(response.response_set, response_set)
+
+    def test_has_value_as_date(self):
+        response_set = ResponseSetFactory()
+        response = DateOfBirthResponse.objects.create(
+            response_set=response_set,
+            value=date(2000, 9, 8)
+        )
+
+        self.assertIsInstance(response.value, date)
