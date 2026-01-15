b644e1f096068dd901e690229a171a081d2eab5e
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2026-01-15 15:37:20 +0000

    PPHA-409: Cleanup family age when diagnosed when answering family history

--- lung_cancer_screening/questions/tests/unit/models/test_family_history_lung_cancer_response.py
+++ lung_cancer_screening/questions/tests/unit/models/test_family_history_lung_cancer_response.py
@@ -1,9 +1,17 @@
 from django.test import TestCase, tag
 
 from ...factories.response_set_factory import ResponseSetFactory
-from ...factories.family_history_lung_cancer_response_factory import FamilyHistoryLungCancerResponseFactory, FamilyHistoryLungCancerValues
+from ...factories.family_history_lung_cancer_response_factory import (
+    FamilyHistoryLungCancerResponseFactory,
+)
+from ...factories.relatives_age_when_diagnosed_response_factory import (
+    RelativesAgeWhenDiagnosedResponseFactory
+)
 
-from ....models.family_history_lung_cancer_response import FamilyHistoryLungCancerResponse
+from ....models.family_history_lung_cancer_response import (
+    FamilyHistoryLungCancerResponse,
+    FamilyHistoryLungCancerValues,
+)
 
 @tag("FamilyHistoryLungCancer")
 class TestFamilyHistoryLungCancerResponse(TestCase):
@@ -16,19 +24,59 @@ class TestFamilyHistoryLungCancerResponse(TestCase):
 
 
     def test_has_response_set_as_foreign_key(self):
-        response_set = ResponseSetFactory()
         response = FamilyHistoryLungCancerResponse.objects.create(
-            response_set=response_set,
+            response_set=self.response_set,
             value=FamilyHistoryLungCancerValues.NO
         )
 
-        self.assertEqual(response.response_set, response_set)
+        self.assertEqual(response.response_set, self.response_set)
 
     def test_has_value_as_string(self):
-        response_set = ResponseSetFactory()
         response = FamilyHistoryLungCancerResponse.objects.create(
-            response_set=response_set,
+            response_set=self.response_set,
             value=FamilyHistoryLungCancerValues.NO
         )
 
         self.assertIsInstance(response.value, str)
+
+
+    def test_deletes_family_age_when_diagnosed_if_response_is_no(self):
+        RelativesAgeWhenDiagnosedResponseFactory.create(response_set=self.response_set)
+
+        self.assertTrue(hasattr(self.response_set, "relatives_age_when_diagnosed"))
+
+        FamilyHistoryLungCancerResponse.objects.create(
+            response_set=self.response_set,
+            value=FamilyHistoryLungCancerValues.NO
+        )
+
+        self.response_set.refresh_from_db()
+        self.assertFalse(hasattr(self.response_set, "relatives_age_when_diagnosed"))
+
+
+    def test_deletes_family_age_when_diagnosed_if_response_is_unknown(self):
+        RelativesAgeWhenDiagnosedResponseFactory.create(response_set=self.response_set)
+
+        self.assertTrue(hasattr(self.response_set, "relatives_age_when_diagnosed"))
+
+        FamilyHistoryLungCancerResponse.objects.create(
+            response_set=self.response_set,
+            value=FamilyHistoryLungCancerValues.UNKNOWN
+        )
+
+        self.response_set.refresh_from_db()
+        self.assertFalse(hasattr(self.response_set, "relatives_age_when_diagnosed"))
+
+
+    def test_does_not_delete_family_age_when_diagnosed_if_response_is_yes(self):
+        RelativesAgeWhenDiagnosedResponseFactory.create(response_set=self.response_set)
+
+        self.assertTrue(hasattr(self.response_set, "relatives_age_when_diagnosed"))
+
+        FamilyHistoryLungCancerResponse.objects.create(
+            response_set=self.response_set,
+            value=FamilyHistoryLungCancerValues.YES
+        )
+
+        self.response_set.refresh_from_db()
+        self.assertTrue(hasattr(self.response_set, "relatives_age_when_diagnosed"))
