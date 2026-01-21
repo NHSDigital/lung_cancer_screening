be81d70ab1a85e4c0b7fcf613035d52fe69c3191
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2026-01-21 09:30:47 +0000

    PPHA-472: Update sex at birth content and new intersex option

--- lung_cancer_screening/questions/views/sex_at_birth.py
+++ lung_cancer_screening/questions/views/sex_at_birth.py
@@ -6,7 +6,7 @@ from ..models.sex_at_birth_response import SexAtBirthResponse
 
 
 class SexAtBirthView(QuestionBaseView):
-    template_name = "question_form.jinja"
+    template_name = "sex_at_birth.jinja"
     form_class = SexAtBirthForm
     model = SexAtBirthResponse
     success_url = reverse_lazy("questions:gender")
