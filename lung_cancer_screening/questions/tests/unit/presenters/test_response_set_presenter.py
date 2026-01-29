09a7ab16722049725f8c6cd27c7ba142d5cc2d79
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2026-01-29 10:06:23 +0000

    PPHA-534: Add smoked total years for cigarette type

--- lung_cancer_screening/questions/tests/unit/presenters/test_response_set_presenter.py
+++ lung_cancer_screening/questions/tests/unit/presenters/test_response_set_presenter.py
@@ -462,7 +462,7 @@ class TestResponseSetPresenter(TestCase):
             response_set=self.response_set, type=TobaccoSmokingHistoryTypes.CIGARETTES
         )
         smoked_total_years_response = SmokedTotalYearsResponseFactory.create(
-            tobacco_smoking_history=cigarettes
+            tobacco_smoking_history=cigarettes,
         )
 
         presenter = ResponseSetPresenter(self.response_set)
