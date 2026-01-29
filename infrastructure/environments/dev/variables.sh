6b3d3e604312139651d28a82b3d142b9e1443536
Alastair Lock <alastair.lock@gmail.com>
2026-01-29 13:59:26 +0000

    wip

--- infrastructure/environments/dev/variables.sh
+++ infrastructure/environments/dev/variables.sh
@@ -1,9 +1,12 @@
+REGION=UK South
+APP_SHORT_NAME=lungcs
 ENVIRONMENT=dev
 ENV_CONFIG=dev
 AZURE_SUBSCRIPTION="Lung Cancer Risk Check - Dev"
 HUB_SUBSCRIPTION="Lung Cancer Risk Check - Non-live hub"
 HUB=nonlive-hub
 TERRAFORM_MODULES_REF=main
+STORAGE_ACCOUNT_RG=rg-hub-nonlive-uks-bootstrap
 ENABLE_SOFT_DELETE=false
 ADO_MANAGEMENT_POOL=private-pool-hub-nonlive-uks
 RUN_NOTIFICATIONS_SMOKE_TEST=true
