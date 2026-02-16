63f3008913ce2cc0cb57e6263ae430a72eb8071f
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2026-02-16 09:00:38 +0000

    Ensure email is present in NHS login scopes

--- lung_cancer_screening/settings.py
+++ lung_cancer_screening/settings.py
@@ -233,7 +233,7 @@ OIDC_OP_JWKS_ENDPOINT = f"{OIDC_OP_FQDN}/.well-known/jwks.json"
 # NHS Login requires RS512 for token endpoint authentication
 # See: https://auth.sandpit.signin.nhs.uk/.well-known/openid-configuration
 OIDC_RP_SIGN_ALGO = "RS512"
-OIDC_RP_SCOPES = "openid profile profile_extended"
+OIDC_RP_SCOPES = "openid profile profile_extended email"
 OIDC_RP_REDIRECT_URI = "/oidc/callback/"
 NHS_LOGIN_SETTINGS_URL = environ.get("NHS_LOGIN_SETTINGS_URL")
 
