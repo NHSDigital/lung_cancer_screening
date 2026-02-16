e446c7f188dd360d88ac293a0ed30d23df4fb739
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2026-02-16 08:41:09 +0000

    Add validation tests for height

--- lung_cancer_screening/questions/tests/unit/models/test_height_response.py
+++ lung_cancer_screening/questions/tests/unit/models/test_height_response.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, tag
+from django.core.exceptions import ValidationError
 
 from ...factories.response_set_factory import ResponseSetFactory
 from ...factories.height_response_factory import HeightResponseFactory
@@ -17,28 +18,118 @@ class TestHeightResponse(TestCase):
 
 
     def test_has_response_set_as_foreign_key(self):
-        response_set = ResponseSetFactory()
-        response = HeightResponse.objects.create(
-            response_set=response_set,
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
             metric=1700
         )
 
-        self.assertEqual(response.response_set, response_set)
+        self.assertEqual(response.response_set, self.response_set)
+
 
     def test_has_metric_as_int(self):
-        response_set = ResponseSetFactory()
-        response = HeightResponse.objects.create(
-            response_set=response_set,
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
             metric=1700
         )
 
         self.assertIsInstance(response.metric, int)
 
     def test_has_imperial_as_int(self):
-        response_set = ResponseSetFactory()
-        response = HeightResponse.objects.create(
-            response_set=response_set,
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
             imperial=68
         )
 
         self.assertIsInstance(response.imperial, int)
+
+
+    def test_is_invalid_if_neither_metric_nor_imperial_are_set(self):
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
+            metric=None,
+            imperial=None
+        )
+
+        with self.assertRaises(ValidationError) as context:
+            response.full_clean()
+
+        self.assertIn(
+            "Either metric or imperial height must be provided.",
+            context.exception.messages,
+        )
+
+
+    def test_is_invalid_if_both_metric_and_imperial_are_set(self):
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
+            metric=1700,
+            imperial=68
+        )
+
+        with self.assertRaises(ValidationError) as context:
+            response.full_clean()
+
+        self.assertIn(
+            "Cannot provide both metric and imperial height.",
+            context.exception.messages,
+        )
+
+
+    def test_is_invalid_if_metric_is_less_than_minimum_height(self):
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
+            metric=HeightResponse.MIN_HEIGHT_METRIC - 1
+        )
+
+        with self.assertRaises(ValidationError) as context:
+            response.full_clean()
+
+        self.assertIn(
+            "Height must be between 139.7cm and 243.8 cm",
+            context.exception.messages,
+        )
+
+
+    def test_is_invalid_if_metric_is_greater_than_maximum_height(self):
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
+            metric=HeightResponse.MAX_HEIGHT_METRIC + 1
+        )
+
+        with self.assertRaises(ValidationError) as context:
+            response.full_clean()
+
+        self.assertIn(
+            "Height must be between 139.7cm and 243.8 cm",
+            context.exception.messages,
+        )
+
+
+    def test_is_invalid_if_imperial_is_less_than_minimum_height(self):
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
+            imperial=HeightResponse.MIN_HEIGHT_IMPERIAL - 1
+        )
+
+        with self.assertRaises(ValidationError) as context:
+            response.full_clean()
+
+        self.assertIn(
+            "Height must be between 4 feet 7 inches and 8 feet",
+            context.exception.messages,
+        )
+
+
+    def test_is_invalid_if_imperial_is_greater_than_maximum_height(self):
+        response = HeightResponseFactory.build(
+            response_set=self.response_set,
+            imperial=HeightResponse.MAX_HEIGHT_IMPERIAL + 1
+        )
+
+        with self.assertRaises(ValidationError) as context:
+            response.full_clean()
+
+        self.assertIn(
+            "Height must be between 4 feet 7 inches and 8 feet",
+            context.exception.messages,
+        )
