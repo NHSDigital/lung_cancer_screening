8125c9be602a50b78625d52fb66d688b80d19cb4
Alastair Lock <alastair.lock@gmail.com>
2026-02-26 13:55:25 +0000

    wip

--- infrastructure/modules/container-apps/jobs.tf
+++ infrastructure/modules/container-apps/jobs.tf
@@ -14,7 +14,6 @@ module "db_setup" {
   docker_image     = var.docker_image
   user_assigned_identity_ids = flatten([
     [module.azure_blob_storage_identity.id],
-    [module.azure_queue_storage_identity.id],
     var.deploy_database_as_container ? [] : [module.db_connect_identity[0].id]
   ])
   environment_variables = merge(
