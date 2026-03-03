c8f1af64ae17b4bda57eafe604187306443bbf1a
Alastair Lock <alastair.lock@gmail.com>
2026-03-03 14:53:15 +0000

    adding sha

--- lung_cancer_screening/urls.py
+++ lung_cancer_screening/urls.py
@@ -14,11 +14,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.contrib.auth.decorators import login_not_required
+from django.http import HttpResponse
 from django.urls import path, include
+from django.views.decorators.http import require_GET
+
+from lung_cancer_screening.core.decorators import basic_auth_exempt
+
+@require_GET
+@basic_auth_exempt
+@login_not_required
+def sha_view(request):
+    return HttpResponse(settings.COMMIT_SHA)
+
+
+@require_GET
+@basic_auth_exempt
+@login_not_required
+def health_check(request):
+    return HttpResponse("OK")
+
 
 urlpatterns = [
     path('', include(
         ("lung_cancer_screening.questions.urls", "questions"),
         namespace="questions")),
     path('oidc/', include('mozilla_django_oidc.urls')),
+    path(
+        "healthcheck",
+        health_check,
+    ),
+    path(
+        "sha",
+        sha_view,
+    ),
 ]
