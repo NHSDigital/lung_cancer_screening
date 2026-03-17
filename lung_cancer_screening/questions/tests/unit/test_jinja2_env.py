ade3ba2bf33cf404ae9a9957bcc7de0d5c4b2014
Jamie Falcus <50366804+jamiefalcus@users.noreply.github.com>
2026-03-17 12:17:00 +0000

    PPHA-679: More copilot fixes

--- lung_cancer_screening/questions/tests/unit/test_jinja2_env.py
+++ lung_cancer_screening/questions/tests/unit/test_jinja2_env.py
@@ -1,9 +1,8 @@
 
 import os
 
-from django.conf import settings
-
-from django.test import TestCase, tag
+from django.test import TestCase, override_settings, tag
+from unittest.mock import patch
 
 from lung_cancer_screening.jinja2_env import get_env_value
 
@@ -11,22 +10,21 @@ from lung_cancer_screening.jinja2_env import get_env_value
 class TestJinja2Env(TestCase):
     def test_get_env_value_with_env(self):
         # Test with an environment variable
-        os.environ['TEST_ENV_VAR'] = 'env_value'
-        self.assertEqual(get_env_value('TEST_ENV_VAR'), 'env_value')
-
+        with patch.dict(os.environ, {"TEST_ENV_VAR": "env_value"}, clear=False):
+            self.assertEqual(get_env_value("TEST_ENV_VAR"), "env_value")
+    @override_settings(TEST_SETTING="setting_value")
     def test_get_env_value_with_setting(self):
         # Test with a Django setting
-        settings.TEST_SETTING = 'setting_value'
         self.assertEqual(get_env_value('TEST_SETTING'), 'setting_value')
 
     def test_get_env_value_without_a_value(self):
         # Test with a non-existent key
         self.assertEqual(get_env_value('NON_EXISTENT_KEY'), 'Value not found')
 
+    @override_settings(TEST_BOTH="setting_value")
     def test_get_env_value_with_env_with_both(self):
         # Test environment variable takes precedence over Django setting
-        os.environ['TEST_BOTH'] = 'env_value'
-        settings.TEST_BOTH = 'setting_value'
-        self.assertEqual(get_env_value('TEST_BOTH'), 'env_value')
+        with patch.dict(os.environ, {"TEST_BOTH": "env_value"}, clear=False):
+            self.assertEqual(get_env_value("TEST_BOTH"), "env_value")
 
 
