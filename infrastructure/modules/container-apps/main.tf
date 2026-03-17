dcf44dcbbdfb82259c9c31efafa0cce66ca8e1b1
Alastair Lock <alastair.lock@gmail.com>
2026-03-17 19:48:13 +0000

    Fix to the allowed hosts
    This adds the default domain to the allowed hosts as well as localhost.

--- infrastructure/modules/container-apps/main.tf
+++ infrastructure/modules/container-apps/main.tf
@@ -31,7 +31,7 @@ module "webapp" {
   environment_variables = merge(
     local.common_env,
     {
-      ALLOWED_HOSTS        = "${local.hostname}",
+      ALLOWED_HOSTS        = "${local.hostname},${var.app_short_name}-web-${var.environment}.${var.default_domain},localhost",
       CSRF_TRUSTED_ORIGINS = "https://${local.hostname}"
     },
     var.deploy_database_as_container ? local.container_db_env : local.azure_db_env
