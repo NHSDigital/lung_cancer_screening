129da1160806f23bb37123764286825f2c71e79c
stephhou <steph.housden1@nhs.net>
2026-04-10 17:00:38 +0100

    fix: fixed comments

--- scripts/sql/export_valid_responses.sql
+++ scripts/sql/export_valid_responses.sql
@@ -1,5 +1,5 @@
 -- SQL script to export a csv file containing NHS numbers of valid responses to a CSV file on to AVD.
--- A valid response is defined as a response where the user has either submitted a response set or has a response to the question "Do you think you need to see a doctor about your symptoms?" with a value of "Yes".
+-- A valid response is defined as a response where the user has either submitted the questionnaire or has a response to the question "Do you need a face to face appointment?" with a value of "Yes".
 -- Run script using psql command line tool or pgAdmin query tool
 
 \set ON_ERROR_STOP on
