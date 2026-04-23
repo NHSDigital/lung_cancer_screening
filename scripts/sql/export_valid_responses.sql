41807216e03ee4cc5d2fd263e8593674fc182d1b
stephhou <167300771+stephhou@users.noreply.github.com>
2026-04-23 14:58:05 +0100

    fix: update file path and comments

--- scripts/sql/export_valid_responses.sql
+++ scripts/sql/export_valid_responses.sql
@@ -1,7 +1,8 @@
 -- SQL script to export a csv file containing NHS numbers of submitted responses to a folder on an AVD.
 -- A valid response is defined as a response where the user has submitted the questionnaire.
--- Run script using the psql command line tool on AVD; this file uses psql meta-commands (\set, \copy) and will not run in pgAdmin Query Tool.
+-- Run script using the psql command line tool on AVD.
+-- Update PATH_TO_EXPORT_FILE before running to specify the location and name of the exported file. The file will be created if it does not exist, or overwritten if it does.
 
 \set ON_ERROR_STOP on
-\copy (SELECT DISTINCT qu.nhs_number FROM questions_user qu JOIN questions_responseset qrs ON qrs.user_id = qu.id WHERE qrs.submitted_at IS NOT NULL) TO 'C:/Users/*YourUsername*/Documents/valid_responses.csv' WITH (FORMAT csv, HEADER true);
+\copy (SELECT DISTINCT qu.nhs_number FROM questions_user qu JOIN questions_responseset qrs ON qrs.user_id = qu.id WHERE qrs.submitted_at IS NOT NULL) TO 'PATH_TO_EXPORT_FILE' WITH (FORMAT csv, HEADER true);
 
