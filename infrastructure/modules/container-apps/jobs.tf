c2516e2cc3cfa34ab9c522fc08ffa614ce76e28e
Alastair Lock <alastair.lock@gmail.com>
2026-04-30 16:24:34 +0100

    As the only scheduled job is alerting put a boolean option around it

--- infrastructure/modules/container-apps/jobs.tf
+++ infrastructure/modules/container-apps/jobs.tf
@@ -40,6 +40,9 @@ module "db_setup" {
 }
 
 module "scheduled_jobs" {
+  # TODO: This should be on a scheduled job bases and NOT for the entire module
+  count = var.enable_alerting ? 1 : 0
+
   source = "../dtos-devops-templates/infrastructure/modules/container-app-job"
 
   for_each = local.scheduled_jobs
